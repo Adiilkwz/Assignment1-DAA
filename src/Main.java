cef3beca0c620d40c117808bf7b40b9eb57ed842
islamamangeldi <240860@astanait.edu.kz>
2025-09-22 18:02:59 +0500

    mergesort

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,55 @@ public class Main {
     }
 
     public static void main(String[] args) {
-            //quicksort testing
-            int n = 20; // размер массива
-            Random rnd = new Random();
-            int[] arr = rnd.ints(n, -50, 50).toArray();
+        //quicksort testing
+        int n = 20; // размер массива
+        Random rnd = new Random();
+        int[] arr = rnd.ints(n, -50, 50).toArray();
 
-            System.out.println("Исходный массив: " + Arrays.toString(arr));
+        System.out.println("Исходный массив: " + Arrays.toString(arr));
 
-            SortMetrics m = new SortMetrics();
-            QuickSort.sort(arr, m);
+        SortMetrics m = new SortMetrics();
+        QuickSort.sort(arr, m);
 
-            System.out.println("Отсортированный массив: " + Arrays.toString(arr));
-            System.out.println("Сравнений: " + m.getComparisons());
-            System.out.println("Перестановок: " + m.getSwaps());
-            System.out.println("Макс. глубина рекурсии: " + m.getMaxDepth());
+        System.out.println("Отсортированный массив: " + Arrays.toString(arr));
+        System.out.println("Сравнений: " + m.getComparisons());
+        System.out.println("Перестановок: " + m.getSwaps());
+        System.out.println("Макс. глубина рекурсии: " + m.getMaxDepth());
+
+        //2. MergeSort
+        Random rand = new Random();
+        for (int s : new int[]{10, 100, 1000, 10000}) {
+            int[] array = new int[s];
+            for (int i = 0; i < s ; i++) array[i] = rand.nextInt(100000);
+
+            MergeSort ms = new MergeSort(s);
+            ms.sort(array);
+
+            System.out.println("Random s=" + s +
+                    " sorted=" + MergeSort.isSorted(array) +
+                    " maxDepth=" + ms.getMaxDepth());
+        }
+
+        int s = 1000;
+        int[] adversarial = new int[s];
+        for (int i = 0; i < s; i++) adversarial[i] = s - i;
+
+        MergeSort ms2 = new MergeSort(s);
+        ms2.sort(adversarial);
+
+        System.out.println("Adversarial s=" + s +
+                " sorted=" + MergeSort.isSorted(adversarial) +
+                " maxDepth=" + ms2.getMaxDepth());
+
+        int[] sorted = new int[s];
+        for (int i = 0; i < s; i++) sorted[i] = i;
+
+        MergeSort ms3 = new MergeSort(s);
+        ms3.sort(sorted);
+
+        System.out.println("Sorted input s=" + s +
+                " sorted=" + MergeSort.isSorted(sorted) +
+                " maxDepth=" + ms3.getMaxDepth());
 
         // Small test for correctness closest pair of points
         int smallN = 1000;
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,2 +1,74 @@
+import java.util.Arrays;
+
 public class MergeSort {
+    private static final int CUTOFF = 16;
+    private final int[] buffer;
+    private int maxDepth;
+
+    public MergeSort(int n) {
+        this.buffer = new int[n];
+    }
+
+    public void sort(int[] arr) {
+        maxDepth = 0;
+        sort(arr, 0, arr.length - 1, 1);
+    }
+
+    private void sort(int[] arr, int left, int right, int depth) {
+        maxDepth = Math.max(maxDepth, depth);
+
+        if (right - left <= CUTOFF) {
+            insertionSort(arr, left, right);
+            return;
+        }
+
+        int mid = (left + right) >>> 1;
+        sort(arr, left, mid, depth + 1);
+        sort(arr, mid + 1, right, depth + 1);
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        System.arraycopy(arr, left, buffer, left, mid - left + 1);
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (buffer[i] <= arr[j]) {
+                arr[k++] = buffer[i++];
+            } else {
+                arr[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            arr[k++] = buffer[i++];
+        }
+    }
+
+    private void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    // Проверка корректности сортировки
+    public static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) return false;
+        }
+        return true;
+    }
 }
